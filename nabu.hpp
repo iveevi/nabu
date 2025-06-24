73e39889cbf9076911e2b386cf0675e3316ba6e5
Venkataram Sivaram <vesion4690@gmail.com>
2025-06-23 23:18:42 -0700

    Syntactic sugar for conversions

--- nabu.hpp
+++ nabu.hpp
@@ -849,3 +849,37 @@ bestd::optional <Container> double_quote_string(const std::string &source, size_
 }
 
 } // namespace nabu
+
+namespace nabu {
+
+// Syntactic sugar
+template <auto f, bool Chain = false>
+struct ctime_wrapper {
+	static constexpr auto handle = f;
+	static constexpr bool chain = Chain;
+};
+
+struct ctime_handle {};
+
+template <typename T, size_t ... Is>
+struct convert_indicator {};
+
+template <auto f, typename T, size_t ... Is>
+constexpr auto operator%(ctime_wrapper <f>, convert_indicator <T, Is...>)
+{
+	constexpr auto ff = nabu::convert <f, T, Is...>;
+	return ctime_wrapper <ff> ();
+}
+
+template <auto f>
+constexpr auto operator<<(ctime_wrapper <f>, ctime_handle)
+{
+	return f;
+}
+
+} // namespace nabu
+
+#define pf(...)		nabu::ctime_wrapper <__VA_ARGS__> ()
+#define as(...)		% nabu::convert_indicator <__VA_ARGS__> ()
+#define as_finally(...)	% nabu::convert_indicator <__VA_ARGS__> () << nabu::ctime_handle()
+#define pf_chain(...)	pf(chain <__VA_ARGS__>)
